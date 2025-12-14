b0f1b68ffea9ebcbb5f59276628a65e16fcdcaac
himanshubora7 <himanshubora972@gmail.com>
2025-12-14 12:01:41 +0530

    day02

---
+++ day02/index.js
@@ -0,0 +1,23 @@
+console.log("Day 02");
+
+var run = "fast";
+
+console.log(run);
+
+var run = "slow";
+console.log(run);
+
+let run2 = "slow";
+console.log(run2);
+
+// let run2 = "fast";
+// this will generate error
+// using let u can only reassign the same variable not re decalre it
+// objects ~ key value pairs
+
+let student = {
+  name: "rahul",
+  age: 22,
+  isEnrolled: true,
+};
+console.log(student.name);
