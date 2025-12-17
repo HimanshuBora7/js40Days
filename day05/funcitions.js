b5e8f2ecd029e399afdf9355c5412606f26e4206
himanshubora7 <himanshubora972@gmail.com>
2025-12-17 11:29:45 +0530

    functions~ completed

--- day05/funcitions.js
+++ day05/funcitions.js
@@ -91,3 +91,164 @@ calculateThis(1, 2, 3, 4, 5, 6, 7, 8, 9);
 // rest parameter should always be used as last parameter , it can't be used as first or middle parameter
 
 // nested function ~ function inside another function
+function outer() {
+  console.log("outer function ");
+
+  function inner() {
+    console.log("inner function ");
+  }
+  inner();
+}
+
+// if we want to execute the inner function it has to be executed inside the outer function because its scope or existence is not beyond outer function
+outer();
+
+//if we want to execute the inner function independently we can return it from the outer function and store it in a variable and execute that variable
+
+function outer2() {
+  console.log("outer function ");
+
+  return function inner2() {
+    console.log("inner function ");
+  };
+}
+
+let ret_func = outer2();
+
+console.log(ret_func);
+ret_func();
+
+// callback function ~ a function that can be passed to another function as argument and call that function we are passing after some time based on some condition
+
+const is_func = true;
+
+function foo(func) {
+  console.log("foo");
+
+  // callback function are called based on some condition or buisness case or buisness logic
+
+  if (is_func) {
+    func();
+    // here we have declared a function as parameter and it is getting executed here
+  }
+}
+
+// here we are passing a function as argument but we have not named it anything as we are executing its body directly inside another function and we don't feel any need to call it somewhere else so it is an anonymous function
+
+foo(function () {
+  console.log("buzzzz");
+});
+
+// named function can also be passed
+
+let hush = function () {
+  console.log("hush");
+};
+
+foo(hush);
+
+// who calls the callback func ?~ some condition, buisness logic or user case
+
+// Pure function ~ function providing same output for same input
+
+function greeting(name) {
+  return "hello, " + name;
+}
+
+console.log(greeting("bochan"));
+console.log(greeting("bochan"));
+console.log(greeting("bochan"));
+// for same input the output will always remains same, ao this is a pure function
+
+//  example of impure function
+
+const greetingMsg = "hello ";
+
+function greeting2(name) {
+  return greetingMsg + name;
+}
+
+console.log(greeting2("bochan"));
+// here the output will always not remain same for same input it will change if the value of greetingMsg is changed, hence this function is impure function
+
+// Higher Order function ~ the function which is either taking other function as an argument or parameter or returning another function is called HOF
+
+function getCamera(camera) {
+  camera();
+}
+// one part of HOF
+getCamera(function () {
+  console.log("nikon ");
+});
+
+// another part of HOF
+
+function returnFunc() {
+  return function () {
+    console.log("returned from function ");
+  };
+}
+
+let ret_func1 = returnFunc();
+ret_func1();
+
+// it is mostly useful when we have to create wrappers
+
+// Arrow function ~ syntax that was introduced in modern js
+
+// normal way of writing a function
+function greetMe() {
+  console.log("hello");
+}
+
+// converting this into arrow function ~ converting function to an expression
+
+let greetMe1 = () => {
+  console.log("hello from arrow function ");
+};
+
+// executing the function
+greetMe1();
+greetMe();
+
+// if the body has one statement we can omit the braces
+
+let x2 = () => console.log("hello ji ");
+
+x2();
+
+// parameter can be declared inside parenthesis
+
+let x3 = (greetingMsg) => console.log(greetingMsg);
+
+x3("hola amigo ");
+
+// wht if we have to return somethig, if we want to return single line of code then we can omit the return keyword
+
+let x4 = () => "holo amigos";
+
+let ret_St = x4();
+console.log(ret_St);
+
+// how this keyword behaves differently for arrow function and why ??
+
+// IIFE ~ (Immediatly Invoked Function Expression )
+
+// () ~ group operator
+
+// we want to immediately invoke a function after its creation or we want to execute a anonymous function
+// wrap the function b/w two parenthesis and and that with two parenthesis ();
+
+(function () {
+  console.log("IIFE");
+})();
+
+// whenever we want something independent on page to execute itself as it is not dependent on dom tree like some plugins or a chatbot which is independent from the html and we want it to be executing as soon as js is loaded
+
+// if we want to pass parameter to IIFE. we pass the argument in ending parenthesis ();
+
+(function (count) {
+  console.log(2 * count);
+})(2);
+
+// call stack
