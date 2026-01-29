d491e090fab7d43cabb1b43cce683a348efd13e1
himanshubora7 <himanshubora972@gmail.com>
2026-01-29 13:14:06 +0530

    add event listener and removal of event listener

--- day18/index.js
+++ day18/index.js
@@ -31,6 +31,7 @@ function handleClick(greeting) {
 const myBtn2Elem = document.getElementById("myBtn2");
 
 myBtn2Elem.onclick = function () {
+  document.querySelector;
   console.log("my btn 2 is clicked ");
 };
 
@@ -42,10 +43,45 @@ myBtn2Elem.onclick = function () {
 
 myBtn2Elem.onclick = () => handleClick("hola amigos");
 
+// trial method
+
+const btnD = document.querySelector("#theme-toggle");
+
+btnD.addEventListener("click", function () {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    btnD.textContent = "light mode";
+  } else {
+    btnD.textContent = "dark mode";
+  }
+});
+
 // add event listener and remove event listener
 
 const countBtnElem = document.getElementById("countBtn");
+let counter = 0;
+
+const display = document.querySelector("#counter-value");
 
 // addEventListener takes 3 parameters, two are mandotary and 1 is optional
 
-countBtnElem.addEventListener();
+// first parameter is about the event we are interested in dealing with
+
+// second parameter is about defining what will happen when the event happens basically a function which will execute when the even happens
+
+// third one is optional which is concerned about evene bubbling
+
+const hadleEvent = function () {
+  console.log("counter", counter);
+  counter++;
+  display.innerText = counter;
+};
+
+countBtnElem.addEventListener("click", hadleEvent);
+
+// by this way(using addEventListener) we can assign mulitple event handler function to single function
+//removing event listener ~ like we want to include some other script so we want to remove events linked with ui from previous script so
+
+// first parameter is same like the event second parameter is event handling function which we wish to remove
+
+countBtnElem.removeEventListener("click", hadleEvent);
