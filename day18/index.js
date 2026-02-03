a4d3c1902c3e95aec5922286c1ead3cd7a203fc2
himanshubora7 <himanshubora972@gmail.com>
2026-02-04 01:21:44 +0530

    event handling prior to event bubbling

--- day18/index.js
+++ day18/index.js
@@ -69,19 +69,63 @@ const display = document.querySelector("#counter-value");
 
 // second parameter is about defining what will happen when the event happens basically a function which will execute when the even happens
 
-// third one is optional which is concerned about evene bubbling
+// third one is optional which is concerned about event bubbling
 
-const hadleEvent = function () {
+const handleEvent = function () {
   console.log("counter", counter);
   counter++;
   display.innerText = counter;
 };
 
-countBtnElem.addEventListener("click", hadleEvent);
+countBtnElem.addEventListener("click", handleEvent);
 
 // by this way(using addEventListener) we can assign mulitple event handler function to single function
 //removing event listener ~ like we want to include some other script so we want to remove events linked with ui from previous script so
 
 // first parameter is same like the event second parameter is event handling function which we wish to remove
+function greeting() {
+  console.log("helloo jii");
+}
+countBtnElem.addEventListener("click", greeting);
 
-countBtnElem.removeEventListener("click", hadleEvent);
+countBtnElem.removeEventListener("click", handleEvent);
+
+// DOM content loaded
+
+// in case we want to check whether the dom content has been loaded or not
+
+//for dom content load event handling we have to  use addEventListener always
+
+// will never run
+document.onDOMcontentLoaded = function () {
+  console.log("dom content loaded ");
+};
+
+// below code works
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("dom content successfully loaded ");
+});
+
+// anatomy of event object
+// everytime event happens we get an event object as part of our event handler function
+// event object is a hidden treasure it helps us in identifying what kind of event is happening
+
+const searchElem = document.getElementById("search-id");
+
+function handleChange(event) {
+  console.log(event); //event object is passed to the function called we can name it anything here we have named it event
+
+  // to know what kind of event has occured
+  console.log("Event type: ", event.type);
+
+  // other helpful properties
+
+  console.log("Target: ", event.target);
+  console.log("Target Name: ", event.target.name);
+  console.log("Target value: ", event.target.value);
+  console.log("Event type: ", event.type);
+  console.log("Current Target: ", event.currentTarget); // it tells abt the element triggered the event
+  console.log(this); // it refers to the element itself on which eventHandler function is added
+}
+
+searchElem.addEventListener("change", handleChange);
